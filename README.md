5f3b970d227d46f0dbad886600888cc2ceb5ecd5
Sam Place <samvauclain@gmail.com>
2021-11-30 20:49:38 -0800

    first set of questions

--- README.md
+++ README.md
@@ -16,7 +16,12 @@ WHEN I enter my project title
 - THEN this is displayed as the title of the README
 
 WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-- THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+- THEN this information is added to the sections of the README entitled:
+    - Description
+    - Installation
+    - Usage
+    - Contributing
+    - Tests
 
 WHEN I choose a license for my application from a list of options
 - THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
--- index.js
+++ index.js
@@ -7,13 +7,78 @@ const inquirer = require('inquirer');
 inquirer.prompt([
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
+        name: 'title',
+        message: 'What is project title? (Required)',
+        validate: titleInput => {
+          if (titleInput) {
             return true;
           } else {
-            console.log('Please enter your name!');
+            console.log('Please enter your project title!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a project description (Required)',
+        validate: descInput => {
+          if (descInput) {
+            return true;
+          } else {
+            console.log('Please enter a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions (Required)',
+        validate: instInput => {
+          if (instInput) {
+            return true;
+          } else {
+            console.log('Please enter installation instructions!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information (Required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please enter usage information!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please list your contribution guidelines (Required)',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please list your contribution guidelines!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please list your test conditions (Required)',
+        validate: testInput => {
+          if (testInput) {
+            return true;
+          } else {
+            console.log('Please list your test conditions!');
             return false;
           }
         }
